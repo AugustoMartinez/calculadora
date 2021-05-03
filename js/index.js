9d2efc23aa48b8a3e6d2dd267e738b6b6575ecaa
AugustoMartinez <82739867+AugustoMartinez@users.noreply.github.com>
2021-05-03 15:39:00 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,25 +1,32 @@
-let display = document.querySelector("#text-current");
+let valorIngresado1 = document.querySelector("#text-current");
+let valorIngresado2 = document.querySelector('#history-text');
 
+//Imprime los caracteres que ingresamos al apretar un boton
 const mostrar = caracter =>{
-    if(display.innerHTML.length<=15){
-        display.innerHTML+=caracter;
-        displat.innerHTML= convertirNumero(display.innerHTML);
+    if(valorIngresado1.innerHTML.length<=15){
+        valorIngresado1.innerHTML+=caracter;
+        valorIngresado2.innerHTML += caracter;
+        valorIngresado1.innerHTML= convertirNumero(valorIngresado1.innerHTML);
     }
     else{
         alert("La cantidad maxima de numeros es 16")
     }
 }
-
+//muestra el resultado de las operaciones realizadas
 const resultado = () =>{
-    display.innerHTML=convertirNumero(display.innerHTML);
+    valorIngresado1.innerHTML=convertirNumero(valorIngresado1.innerHTML);
 }
-
+//elimina todos los valores que se ingresaron
 const limpiar = () =>{
-    display.innerHTML = "0";
+    valorIngresado1.innerHTML = "";
 }
-
+//Les agrega una como a los numeros segun sean miles, millones, etc.
 const convertirNumero = cadena =>{
-    let num=Number(cadena);
+    let num=Number(cadena.replace(/,/g,""));
     let separar= num.toLocaleString("en");
     return separar;
+}
+//borra solo 1 caracter del valor
+const borrarUnValor = () =>{
+    valorIngresado1.innerHTML= valorIngresado1.innerHTML.slice(0,-1);
 }
